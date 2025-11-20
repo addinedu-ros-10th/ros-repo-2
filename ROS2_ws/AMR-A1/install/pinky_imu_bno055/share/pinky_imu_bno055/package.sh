3826880e0a6babbc82dc8756186f77e12949e1f7
korobuster001 <jxlc001@gmail.com>
2025-11-20 10:44:20 +0900

    waypoint 좌표 추가

--- ROS2_ws/AMR-A1/install/pinky_imu_bno055/share/pinky_imu_bno055/package.sh
+++ ROS2_ws/AMR-A1/install/pinky_imu_bno055/share/pinky_imu_bno055/package.sh
@@ -52,7 +52,7 @@ _colcon_prepend_unique_value() {
 # since a plain shell script can't determine its own path when being sourced
 # either use the provided COLCON_CURRENT_PREFIX
 # or fall back to the build time prefix (if it exists)
-_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/lim/pinky_pro/install/pinky_imu_bno055"
+_colcon_package_sh_COLCON_CURRENT_PREFIX="/home/lim/dev_ws/ros-repo-2/ROS2_ws/AMR-A1/install/pinky_imu_bno055"
 if [ -z "$COLCON_CURRENT_PREFIX" ]; then
   if [ ! -d "$_colcon_package_sh_COLCON_CURRENT_PREFIX" ]; then
     echo "The build time path \"$_colcon_package_sh_COLCON_CURRENT_PREFIX\" doesn't exist. Either source a script for a different shell or set the environment variable \"COLCON_CURRENT_PREFIX\" explicitly." 1>&2
