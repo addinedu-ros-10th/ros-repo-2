0165e9cc577e6043e3ee1d68c717b909076672c2
Jimingbori <seojimni@gmail.com>
2025-11-20 20:48:20 +0900

    [ASSET] 변경된 waypoint로 수정함

--- ROS2_ws/AMR-A1/install/pinky_navigation/share/pinky_navigation/local_setup.sh
+++ ROS2_ws/AMR-A1/install/pinky_navigation/share/pinky_navigation/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/lim/dev_ws/ros-repo-2/ROS2_ws/AMR-A1/install/pinky_navigation"}
+: ${AMENT_CURRENT_PREFIX:="/home/jiming/proj_ws/ros-repo-2/ROS2_ws/AMR-A1/install/pinky_navigation"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
